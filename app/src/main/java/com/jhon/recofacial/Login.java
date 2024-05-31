c4bb8deb116c2e52b3954cc71be5a08e97a20c20
RosalesJhordi <1428060@senati.pe>
2024-05-31 09:21:59 -0500

    Actualizacion

--- app/src/main/java/com/jhon/recofacial/Login.java
+++ app/src/main/java/com/jhon/recofacial/Login.java
@@ -1,76 +1,15 @@
 package com.jhon.recofacial;
 
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.widget.Toast;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import com.androchef.cameraxfacedetection.camerax.CameraManager;
+
 
 public class Login extends AppCompatActivity {
-
-    private CameraManager cameraManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        createCameraManager();
-        checkForPermission();
-        onClicks();
-    }
+        setContentView(R.layout.activity_login);
 
-    private void checkForPermission() {
-        if (allPermissionsGranted()) {
-            cameraManager.startCamera();
-        } else {
-            ActivityCompat.requestPermissions(
-                    this,
-                    REQUIRED_PERMISSIONS,
-                    REQUEST_CODE_PERMISSIONS
-            );
-        }
     }
-
-    private void onClicks() {
-        findViewById(R.id.btnSwitch).setOnClickListener(v -> {
-            cameraManager.changeCameraSelector();
-        });
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                cameraManager.startCamera();
-            } else {
-                Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show();
-                finish();
-            }
-        }
-    }
-
-    private void createCameraManager() {
-        cameraManager = new CameraManager(
-                this,
-                findViewById(R.id.previewView_finder),
-                this,
-                findViewById(R.id.graphicOverlay_finder)
-        );
-    }
-
-    private boolean allPermissionsGranted() {
-        for (String permission : REQUIRED_PERMISSIONS) {
-            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private static final int REQUEST_CODE_PERMISSIONS = 10;
-    private static final String[] REQUIRED_PERMISSIONS = new String[]{android.Manifest.permission.CAMERA};
 }
